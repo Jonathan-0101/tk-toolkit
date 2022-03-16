612936cb73bdd1278bd0f6abd0e38e816e4cd210
Jonathan-0101 <jonathan.woolf.01@gmail.com>
2022-03-16 22:14:14 +0000

    fixing merge issues

--- examples/menu maker example.py
+++ examples/menu maker example.py
@@ -7,7 +7,7 @@ def function1(prameterList1):
 
 #Creating base window   
 root = tk.Tk()
-root.geometry('300x300')
+root.geometry('360x500')
 root.title('Menu')
 
 parameter1 = "parameter1"
@@ -19,9 +19,7 @@ prameterList1 = (parameter1, parameter2)
 #Creating a 2D array for the buttons, with the text for button, function and the parameters
 buttonList = [["Button 1",function1, prameterList1]]
 
-#Calling the function, passing through the tkinter window and button list
-tkm.menuMaker(root, buttonList, 7, 1)
+#Calling the function, passing throught the tkinter window, button list and size for buttons 
+tkm.menuMaker(root, buttonList, 4, 1)
 
 root.mainloop()
-
-        
