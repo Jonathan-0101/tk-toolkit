0089f5623e7e0f924cdecf9419ceb0aba260a7f8
Matthew Kayne <matthewkayne2004@gmail.com>
2022-03-16 22:55:27 +0000

    added webcam example

--- example.py
+++ example.py
@@ -1,1 +1,13 @@
-from tk_toolkit import *
+import tkinter as tk
+from tkinter import *
+from tk_toolkit import tk_toolkit
+
+window = tk.Tk() # Sets up GUI
+window.title("Example") # Titles GUI
+window.geometry("1000x1000") # Sizes GUI
+
+# Webcam
+video = tk_toolkit.Webcam(window, 450, 450) # Uses Box class from webcam to create video window
+video.show_frames() # Show the created Box
+
+tk.mainloop()
