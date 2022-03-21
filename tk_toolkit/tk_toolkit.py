e4cc5fd32e8546f663942be37c4786948c0b26da
Matthew Kayne <matthewkayne2004@gmail.com>
2022-03-21 10:44:39 +0000

    changed object parameters to variables

--- tk_toolkit/tk_toolkit.py
+++ tk_toolkit/tk_toolkit.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 from tkinter import *
-import cv2 
+import cv2
 from PIL import Image, ImageTk
 
+
 class Webcam:
-    def __init__(self, window, width, height, camera=0):
+    def __init__(self, window, width=450, height=450, camera=0):
         self.window = window
         self.width = width
         self.height = height
@@ -12,26 +13,30 @@ class Webcam:
         self.cap = cv2.VideoCapture(camera)
         self.label.pack()
 
-    def show_frames(self): # Define function to show frame
+    def show_frames(self):  # Define function to show frame
 
-        cv2image= cv2.cvtColor(self.cap.read()[1],cv2.COLOR_BGR2RGB) # Get the latest frame and convert into Image
+        # Get the latest frame and convert into Image
+        cv2image = cv2.cvtColor(self.cap.read()[1], cv2.COLOR_BGR2RGB)
         img = Image.fromarray(cv2image)
-        
-        imgtk = ImageTk.PhotoImage(image = img) # Convert image to PhotoImage
+
+        imgtk = ImageTk.PhotoImage(image=img)  # Convert image to PhotoImage
 
         self.label.imgtk = imgtk
         self.label.configure(image=imgtk)
-        
-        self.label.after(20, self.show_frames) # Repeat after an interval to capture continiously
+
+        # Repeat after an interval to capture continiously
+        self.label.after(20, self.show_frames)
+
 
 class Menu:
     def __init__(self, root, buttonList, x, y):
-        
+
         self.buttonList = buttonList
         self.root = root
         self.x = x
         self.y = y
-        
+
         for buttonItem in buttonList:
-            button = Button(self.root, text=buttonItem[0], command=lambda: [buttonItem[1](buttonItem[2])], width=x, height=y,pady=10, padx=10)
+            button = Button(self.root, text=buttonItem[0], command=lambda: [
+                            buttonItem[1](buttonItem[2])], width=x, height=y, pady=10, padx=10)
             button.pack(pady=10, padx=10)
